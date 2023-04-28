dd5d496b8eba8c464707cdb06388ab4e463bf289
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 10:51:11 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,21 +5,28 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('path', [
-  ('/usr/local/bin/innotop'),
-])
+@pytest.mark.parametrize(
+    "path",
+    [
+        ("/usr/local/bin/innotop"),
+    ],
+)
 def test_innotop_is_installed(host, path):
     path = host.file(path)
     assert path.exists
     assert path.is_file
 
 
-@pytest.mark.parametrize('command,output', [
-  ('/usr/local/bin/innotop --version', 'innotop  Ver 1.13.0'),
-])
+@pytest.mark.parametrize(
+    "command,output",
+    [
+        ("/usr/local/bin/innotop --version", "innotop  Ver 1.13.0"),
+    ],
+)
 def test_innotop_runs(host, command, output):
     stdout = host.check_output(command)
     assert stdout == output

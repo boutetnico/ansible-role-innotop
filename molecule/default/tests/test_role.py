b876e710d2e3074dc810c5f4fb830d607fa8d6cd
Nicolas Boutet <amd3002@gmail.com>
2024-11-22 11:17:32 +0100

    Update to 1.15.1

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -16,7 +16,7 @@ def test_innotop_is_installed(host, path):
 @pytest.mark.parametrize(
     "command,output",
     [
-        ("/usr/local/bin/innotop --version", "innotop  Ver 1.13.0"),
+        ("/usr/local/bin/innotop --version", "innotop  Ver 1.15.1"),
     ],
 )
 def test_innotop_runs(host, command, output):
